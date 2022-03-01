f54c92826b150718fb83a3c24c1404cf4eaf564f
John Herbener <john.herbener@semsee.com>
2022-02-28 20:46:51 -0500

    [ 10 ] Add TailwindCSS to project and configure
    https://tailwindcss.com/docs/guides/phoenix
    update tailwind css

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :live_view_stripe, LiveViewStripeWeb.Endpoint,
   secret_key_base: "+6Z29FtDe1Un4cd6kFtAsRs3C56tN54uf7T/SGt15sb3lRwv8/zWIOekMYD/SLrS",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule LiveViewStripe.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:stripity_stripe, "~> 2.13.0"}
+      {:stripity_stripe, "~> 2.13.0"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
@@ -66,7 +67,8 @@ defmodule LiveViewStripe.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
